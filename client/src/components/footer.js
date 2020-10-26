eaf94ded15dfb0f7cb8cfa0a0b18f70f1d8d7747
Saket789 <56218641+Saket789@users.noreply.github.com>
2020-10-26 18:19:08 +0530

    Add files via upload

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-// TODO: fix linkdin links
-
-function Footer(){
-    return(
-        <div className='footer'>
-            Developed by <a href="https://linkdin.com">śaki</a> and <a href="https://linkdin.com">àkg</a> 
+/**
+ * footer for all pages
+ */
+function Footer() {
+    return (
+        <div>
+            <div className='footer'>
+                <p>developed by <a href="https://linkdin.com">saki</a> and <a href="https://linkdin.com">akg</a></p>
+            </div>
         </div>
     )
 }
